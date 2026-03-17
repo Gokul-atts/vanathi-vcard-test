02e1580b86082c6713d9ef048ca840b5fa14f54a
Gokul-atts <gokul@atts.in>
2026-03-17 16:45:04 +0530

    chnages

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import  Logo from "../assets/logo.png"
 import  Flower from "../assets/flower.png"
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
@@ -12,6 +13,8 @@ const Header = () => {
         <img src={Flower} alt="phone" />
         <span>உதவி எண் : +91-72 00 33 1442</span>
       </div>
+
+      <Marquee speed={40} pauseOnHover delay={2} gradient={false}><span style={{ paddingRight: "250px" }}>கோவை மக்களுக்கான சேமிப்பை மேம்படுத்த திருமதி வானதி சீனிவாசன் அவர்களால் உருவாக்கப்பட்ட திட்டம்; இதில் பதிவுபெற்ற கடைகளின் தள்ளுபடிகளை பயன்படுத்தி உங்கள் சேமிப்பை உயர்த்துங்கள்.</span></Marquee>
     </header>
   );
 };
